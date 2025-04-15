2c7a8be44bae52708d39c84f65df467dee6e7a11
JRogula <jakub.rogula@microsoft.wsei.edu.pl>
2025-04-15 09:05:17 +0200

    paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/trainme2/',  // Must match your repository name
+  base: '/trainme2/', // Must match your repository name
 })
